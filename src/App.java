4012a3d573546790bece854b26ee80f2f42d3e33
Pablo Tedesco <tedesco8@gmail.com>
2024-10-01 23:19:56 -0300

    se ejecutan queries de ejemplo

--- src/App.java
+++ src/App.java
@@ -1,5 +1,31 @@
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import configuration.DatabaseConnection;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        DatabaseConnection dbConnection = new DatabaseConnection();
+
+        try {
+            // Ejemplo de lectura
+            ResultSet rs = dbConnection.executeQuery("SELECT * FROM tu_tabla");
+            while (rs.next()) {
+                System.out.println("ID: " + rs.getInt("id") + ", Nombre: " + rs.getString("nombre"));
+            }
+
+            // Ejemplo de escritura
+            int rowsAffected = dbConnection.executeUpdate("INSERT INTO tu_tabla (nombre) VALUES ('Nuevo Nombre')");
+            System.out.println("Filas afectadas: " + rowsAffected);
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                dbConnection.closeConnection();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
